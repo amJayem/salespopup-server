d7f92df2091afec26ae1aba94eb7543989875d70
amJayem <csejayem19@gmail.com>
2023-04-14 15:01:20 +0600

    getting data  by shop name

--- routes/getData.js
+++ routes/getData.js
@@ -4,9 +4,10 @@ const getDataSchema = require('../models/getDataModel')
 
 const getData = router.get('/get-data', async (req, res) => {
   try {
-    const shop = req.query
-    console.log(shop)
-    const result = await getDataSchema.find()
+    const shop = req.query.shop
+    const filter = { shop: shop }
+    // console.log(filter)
+    const result = await getDataSchema.find(filter)
     // console.log(result)
     res.status(201).send(result)
   } catch (error) {
